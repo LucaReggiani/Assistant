84f7eacf97661422a6cd0ef3849dd857941c6c6b
LucaReggiani <lucareggiani1997@gmail.com>
2020-11-02 02:53:43 +0100

    improving of assistant, recognition of my voice and printing of queries

--- main.py
+++ main.py
@@ -4,27 +4,38 @@ import speech_recognition as sr
 
 def take_commands():
 
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print('Listening')
+    r = sr.Recognizer()  # The purpose of a Recognizer instance is to recognize speech.
+    r.energy_threshold = 4000
 
-        r.pause_threshold = 0.7
+    with sr.Microphone(device_index=0) as source:
+        print('speak please:')
+
+        # r.pause_threshold = 1
+        audio = r.record(source, duration=5)
+        # audio = r.listen(source)
+    with open("microphone-results.wav", "wb") as f:
+        f.write(audio.get_wav_data())
+
+    with sr.AudioFile("microphone-results.wav") as source:
+        # r.adjust_for_ambient_noise(source, duration=1)
         audio = r.listen(source)
-        print("Ciao")
-        try:
-            print('Recognizing')
-            # for listening the command in indian english
-            Query = r.recognize_google(audio, language='en-in')
-            # for printing the query or the command that we give
-            print("the query is printed='", Query, "'")
+    try:
+        print('Recognizing')
+        # for listening the command in indian english
+        Query = r.recognize_google(audio, language='it_IT')
+        # Query = r.recognize_sphinx(audio, language='it-IT', )
+        # for printing the query or the command that we give
+        print("the query is printed: '", Query, "'")
 
-        except Exception as e:
 
-            # this method is for handling the exception and so that assistant can ask for telling again the command
-            print(e)
-            print("Please, repeat again")
-            return "None"
-        return Query
+    except Exception as e:
+
+        # this method is for handling the exception and so that assistant can ask for telling again the command
+        print(e)
+        print("Please, repeat again")
+        return "None"
+
+    return Query
 
 
 def Speak(audio):
