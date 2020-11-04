fe36a79f589be7c3658615a8bf6fbb52eae6aa28
LucaReggiani <lucareggiani1997@gmail.com>
2020-11-04 01:16:54 +0100

    cleaning of the code, added a file that contains a dictionary with all languages that are supported

--- main.py
+++ main.py
@@ -6,27 +6,37 @@ import os
 def take_commands():
 
     r = sr.Recognizer()  # The purpose of a Recognizer instance is to recognize speech.
+
+    # This threshold is associated with the perceived loudness of the sound, but it is a
+    # nonlinear relationship. The actual energy threshold you will need depends on your
+    # microphone sensitivity or audio data. Typical values for a silent room are 0 to 100,
+    # and typical values for speaking are between 150 and 3500. Ambient noise has a
+    # significant impact on what values will work best. For this reason, it's better set
+    # this property to a high value initially (4000 works well), so the threshold is
+    # always above ambient noise levels.
+
     r.energy_threshold = 4000
 
     with sr.Microphone(device_index=0) as source:
         print('speak please:')
 
         # r.pause_threshold = 1
+
+        # voice registration by the microphone chosen for a period of time = 5. It's possible to change it
         audio = r.record(source, duration=5)
-        # audio = r.listen(source)
+
+    # saving the registration as a .wav file
     with open("microphone-results.wav", "wb") as f:
         f.write(audio.get_wav_data())
 
+    # listening of the file and store it in the 'audio' variable
     with sr.AudioFile("microphone-results.wav") as source:
-        # r.adjust_for_ambient_noise(source, duration=1)
         audio = r.listen(source)
+
     try:
         print('Recognizing')
-        # for listening the command in indian english
-        Query = r.recognize_google(audio, language='it_IT')
-        # Query = r.recognize_sphinx(audio, language='it-IT', )
-        # for printing the query or the command that we give
-        # print("the query is printed: '", Query, "'")
+        # for listening the command in italian
+        query = r.recognize_google(audio, language='it_IT')
 
     except Exception as e:
 
@@ -35,75 +45,41 @@ def take_commands():
         print("Please, repeat again")
         return "None"
 
-    return Query
+    return query
 
 
-def Speak(mytext, understand=False):
+def Speak(mytext):
 
-    # Language in which you want to convert
+    # Language in which you want to convert, in myu case i chosen italian
     language = 'it'
+    final_text = 'You said ' + mytext
 
-    if understand == True:
-        final_phrase = 'Hai detto ' + mytext + '?'
-    else:
-        final_phrase = mytext
     # Passing the text and language to the engine,
     # here we have marked slow=False. Which tells
     # the module that the converted audio should
     # have a high speed
-    try:
-        myobj = gTTS(text=final_phrase, lang=language, slow=False)
-        # Saving the converted audio in a mp3 file named
-        # welcome
-        myobj.save("welcome.mp3")
-    except Exception as ex:
-        return ex
+    myobj = gTTS(text=final_text, lang=language, slow=False)
 
-    else:
-        file = "welcome.mp3"
-        os.system("mpg123 " + file)
+    # Saving the converted audio in a mp3 file named command
+    myobj.save("command.mp3")
+
+    file = "command.mp3"
+
+    # playing the audio file
+    os.system("mpg123 " + file)
+
 
 if __name__ == '__main__':
 
-    # Speak('Ricevuto! apro il dizionario')
     while True:
 
+        # calling function take_commands to listen the voice
         command = take_commands()
-        print(command)
-        if 'calendario'.casefold() in command.casefold():
-            try:
-                Speak(command, understand=True)
-            except Exception as ex:
-                Speak('Spiacente, per un mal funzionameto è necessario ripetere!')
-                continue
 
-        elif 'Spotify'.casefold() in command.casefold():
-            try:
-                Speak(command, understand=True)
+        # repetition of your command
+        Speak(command)
 
-            except Exception as ex:
-                Speak('Spiacente, per un mal funzionameto è necessario ripetere!')
-                continue
-
-        elif 'facebook'.casefold() in command.casefold():
-            try:
-                Speak(command, understand=True)
-
-            except Exception as ex:
-                Speak('Spiacente, per un mal funzionameto è necessario ripetere!')
-                continue
-
-        elif 'google'.casefold() in command.casefold():
-            try:
-                Speak(command, understand=True)
-
-            except Exception as ex:
-                Speak('Spiacente, per un mal funzionameto è necessario ripetere!')
-                continue
-
-        else:
-            try:
-                Speak('Non ho capito! Ripeti per favore')
-            except Exception as ex:
-                Speak('Spiacente, per un mal funzionameto è necessario ripetere!')
-                continue
+        # it's possiblo to say which actions the assistant has to consider with a simple if statement
+        if 'Python'.casefold() in command.casefold():
+            # Instructions here
+            pass
